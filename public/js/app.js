1a9a86c4243a20fe90fafc6b0a3bcf445e18e40c
abdoulkarimgalima <karimoudiallo69@gmail.com>
2020-01-05 17:51:21 +0000

    composerpublic

--- public/js/app.js
+++ public/js/app.js
@@ -49613,8 +49613,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/karimdiallo/www/dloumo/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/karimdiallo/www/dloumo/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/diallo/testgit/projet_diallo1/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/diallo/testgit/projet_diallo1/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
